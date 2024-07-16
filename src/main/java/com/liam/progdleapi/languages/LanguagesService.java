379e41baff571abbf7b8060c8ac4fb7f85f74247
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-16 20:04:32 +0200

    create LanguagesService

---
+++ src/main/java/com/liam/progdleapi/languages/LanguagesService.java
@@ -0,0 +1,33 @@
+package com.liam.progdleapi.languages;
+
+import com.liam.progdleapi.languages.entity.Language;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class LanguagesService {
+
+    private final LanguageRepository languageRepository;
+
+    public LanguagesService(LanguageRepository languageRepository) {
+        this.languageRepository = languageRepository;
+    }
+
+    public List<Language> getAllLanguages() {
+        return languageRepository.findAll();
+    }
+
+    public Language getLanguageById(UUID id) {
+        return languageRepository.findById(id).orElse(null);
+    }
+
+    public Language createLanguage(Language language) {
+        return languageRepository.save(language);
+    }
+
+    public Language findLanguageByName(String name) {
+        return languageRepository.findByName(name);
+    }
+}
