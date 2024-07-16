aec3dd0d10795a41630447f003bde1a5e663d0b7
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-16 20:03:44 +0200

    create GuessDto

---
+++ src/main/java/com/liam/progdleapi/guess/entity/GuessDto.java
@@ -0,0 +1,22 @@
+package com.liam.progdleapi.guess.entity;
+
+import com.liam.progdleapi.guess.GuessService;
+
+public record GuessDto(
+        GuessService.STATES memorySafe,
+        GuessService.STATES domain,
+        GuessService.STATES name,
+        GuessService.STATES os,
+        GuessService.STATES paradigm,
+        GuessService.STATES typing) {
+    public static GuessDto from(Guess guess) {
+        return new GuessDto(
+                guess.getMemorySafe(),
+                guess.getDomain(),
+                guess.getName(),
+                guess.getOs(),
+                guess.getParadigm(),
+                guess.getTyping()
+        );
+    }
+}
