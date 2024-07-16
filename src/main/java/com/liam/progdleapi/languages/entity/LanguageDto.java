4365e670f7c7cf0c929e26f70bd951462052ce12
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-16 20:04:04 +0200

    create LanguageDto

---
+++ src/main/java/com/liam/progdleapi/languages/entity/LanguageDto.java
@@ -0,0 +1,26 @@
+package com.liam.progdleapi.languages.entity;
+
+import java.util.UUID;
+
+public record LanguageDto(
+        UUID id,
+        long releaseYear,
+        boolean memorySafe,
+        String domain,
+        String name,
+        String os,
+        String paradigm,
+        String typing) {
+    public static LanguageDto from(Language language) {
+        return new LanguageDto(
+                language.getId(),
+                language.getReleaseYear(),
+                language.isMemorySafe(),
+                language.getDomain(),
+                language.getName(),
+                language.getOs(),
+                language.getParadigm(),
+                language.getTyping()
+        );
+    }
+}
