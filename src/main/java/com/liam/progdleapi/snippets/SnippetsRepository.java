2154042ae0063ea4edf388f05f0f47cb06ca410f
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-17 11:51:44 +0200

    add findByLanguage method

--- src/main/java/com/liam/progdleapi/snippets/SnippetsRepository.java
+++ src/main/java/com/liam/progdleapi/snippets/SnippetsRepository.java
@@ -1,9 +1,12 @@
 package com.liam.progdleapi.snippets;
 
+import com.liam.progdleapi.languages.entity.Language;
 import com.liam.progdleapi.snippets.entity.Snippet;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface SnippetsRepository extends ListCrudRepository<Snippet, UUID> {
+    List<Snippet> findByLanguage(Language language);
 }
