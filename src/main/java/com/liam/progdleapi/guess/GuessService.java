37cee7a6ec648b2017fe532ff9d60acd2a94257a
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-17 13:35:05 +0200

    get CORRECT_LANGUAGE from LanguagesService

--- src/main/java/com/liam/progdleapi/guess/GuessService.java
+++ src/main/java/com/liam/progdleapi/guess/GuessService.java
@@ -9,72 +9,70 @@ import org.springframework.stereotype.Service;
 public class GuessService {
 
     private final LanguagesService languagesService;
-    private Language CORRECT_LANGUAGE;
 
     private GuessService(LanguagesService languagesService) {
         this.languagesService = languagesService;
-        this.CORRECT_LANGUAGE = languagesService.findLanguageByName("JavaScript");
     }
 
     private STATES validateReleaseYear(Language language) {
-        if (language.getReleaseYear() == CORRECT_LANGUAGE.getReleaseYear()) {
+        if (language.getReleaseYear() == languagesService.CORRECT_LANGUAGE.getReleaseYear()) {
             return STATES.CORRECT;
         }
-        if (language.getReleaseYear() < CORRECT_LANGUAGE.getReleaseYear()) {
+        if (language.getReleaseYear() < languagesService.CORRECT_LANGUAGE.getReleaseYear()) {
             return STATES.PARTIAL;
         }
         return STATES.INCORRECT;
     }
 
     private STATES validateParadigm(Language language) {
-        if (language.getParadigm().equals(CORRECT_LANGUAGE.getParadigm())) {
+        if (language.getParadigm().equals(languagesService.CORRECT_LANGUAGE.getParadigm())) {
             return STATES.CORRECT;
         }
-        if (CORRECT_LANGUAGE.getParadigm().contains(language.getParadigm())) {
+        if (languagesService.CORRECT_LANGUAGE.getParadigm().contains(language.getParadigm())) {
             return STATES.PARTIAL;
         }
         return STATES.INCORRECT;
     }
 
     private STATES validateTyping(Language language) {
-        if (language.getTyping().equals(CORRECT_LANGUAGE.getTyping())) {
+        if (language.getTyping().equals(languagesService.CORRECT_LANGUAGE.getTyping())) {
             return STATES.CORRECT;
         }
-        if (CORRECT_LANGUAGE.getTyping().contains(language.getTyping())) {
+        if (languagesService.CORRECT_LANGUAGE.getTyping().contains(language.getTyping())) {
             return STATES.PARTIAL;
         }
         return STATES.INCORRECT;
     }
 
     private STATES validateDomain(Language language) {
-        if (language.getDomain().equals(CORRECT_LANGUAGE.getDomain())) {
+        if (language.getDomain().equals(languagesService.CORRECT_LANGUAGE.getDomain())) {
             return STATES.CORRECT;
         }
-        if (CORRECT_LANGUAGE.getDomain().contains(language.getDomain())) {
+        if (languagesService.CORRECT_LANGUAGE.getDomain().contains(language.getDomain())) {
             return STATES.PARTIAL;
         }
         return STATES.INCORRECT;
     }
 
     private STATES validateName(Language language) {
-        if (language.getName().equals(CORRECT_LANGUAGE.getName())) {
+        if (language.getName().equals(languagesService.CORRECT_LANGUAGE.getName())) {
             return STATES.CORRECT;
         }
         return STATES.INCORRECT;
     }
 
     private STATES validateOs(Language language) {
-        if (language.getOs().equals(CORRECT_LANGUAGE.getOs())) {
+        if (language.getOs().equals(languagesService.CORRECT_LANGUAGE.getOs())) {
             return STATES.CORRECT;
         }
-        if (CORRECT_LANGUAGE.getOs().contains(language.getOs())) {
+        if (languagesService.CORRECT_LANGUAGE.getOs().contains(language.getOs())) {
             return STATES.PARTIAL;
         }
         return STATES.INCORRECT;
     }
 
     private STATES validateMemorySafe(Language language) {
-        if (language.isMemorySafe() == CORRECT_LANGUAGE.isMemorySafe()) {
+        if (language.isMemorySafe() == languagesService.CORRECT_LANGUAGE.isMemorySafe()) {
             return STATES.CORRECT;
         }
         return STATES.INCORRECT;
@@ -87,10 +85,10 @@ public class GuessService {
     }
 
     public Guess validateGuess(Language language) {
-        if (CORRECT_LANGUAGE == null) {
-            CORRECT_LANGUAGE = this.languagesService.findLanguageByName("JavaScript");
+        if (languagesService.CORRECT_LANGUAGE == null) {
+            languagesService.CORRECT_LANGUAGE = this.languagesService.findLanguageByName("JavaScript");
         }
-        if (language.equals(CORRECT_LANGUAGE)) {
+        if (language.equals(languagesService.CORRECT_LANGUAGE)) {
             return new Guess(
                     STATES.CORRECT,
                     STATES.CORRECT,
@@ -99,7 +97,7 @@ public class GuessService {
                     STATES.CORRECT,
                     STATES.CORRECT,
                     STATES.CORRECT,
-                    CORRECT_LANGUAGE
+                    languagesService.CORRECT_LANGUAGE
             );
         }
 
