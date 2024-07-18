0605566531e90679139743cdfadb9049c2fd09ee
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-18 10:10:14 +0200

    create users service

---
+++ src/main/java/com/liam/progdleapi/users/UsersService.java
@@ -0,0 +1,44 @@
+package com.liam.progdleapi.users;
+
+import com.liam.progdleapi.users.entity.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UsersService {
+
+    private final UsersRepository usersRepository;
+
+    public UsersService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return usersRepository.findAll();
+    }
+
+    public User getUserById(UUID id) {
+        return usersRepository.findById(id).orElse(null);
+    }
+
+    public User updateUser(User user) {
+        User userToUpdate = usersRepository.findById(user.getId()).orElse(null);
+        if (userToUpdate != null) {
+            return null;
+        }
+        return usersRepository.save(user);
+    }
+
+    public User getUserByUsername(String username) {
+        return usersRepository.findByUsername(username);
+    }
+
+    public User createUser(User user) {
+        if (getUserByUsername(user.getUsername()) != null) {
+            return null;
+        }
+        return usersRepository.save(user);
+    }
+}
