e6e74ab5329de374ae348240f6a7ca0b4caa636f
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-18 10:09:36 +0200

    create user entity

---
+++ src/main/java/com/liam/progdleapi/users/entity/User.java
@@ -0,0 +1,33 @@
+package com.liam.progdleapi.users.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @UuidGenerator
+    private UUID id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    private String profilePictureSrc;
+
+    @Column
+    private long score;
+}
