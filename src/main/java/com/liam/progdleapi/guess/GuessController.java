627ce8805c42882c97288ec7a6b55e7b7e164431
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-07-18 13:30:15 +0200

    add basic authentication for score tracking

--- src/main/java/com/liam/progdleapi/guess/GuessController.java
+++ src/main/java/com/liam/progdleapi/guess/GuessController.java
@@ -4,6 +4,9 @@ import com.liam.progdleapi.guess.entity.Guess;
 import com.liam.progdleapi.guess.entity.GuessDto;
 import com.liam.progdleapi.languages.LanguagesService;
 import com.liam.progdleapi.languages.entity.Language;
+import com.liam.progdleapi.users.UsersService;
+import com.liam.progdleapi.users.entity.User;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,21 +16,39 @@ import org.springframework.web.bind.annotation.*;
 public class GuessController {
     private final GuessService guessService;
     private final LanguagesService languagesService;
+    private final UsersService usersService;
 
-    public GuessController(GuessService guessService, LanguagesService languagesService) {
+    public GuessController(
+            GuessService guessService,
+            LanguagesService languagesService,
+            UsersService usersService) {
         this.guessService = guessService;
         this.languagesService = languagesService;
+        this.usersService = usersService;
     }
 
     @GetMapping("/{language}")
-    public ResponseEntity<GuessDto> guess(@PathVariable String language) {
+    public ResponseEntity<GuessDto> guess(
+            @RequestHeader("Authorization") String token,
+            @PathVariable String language) {
         Language guessedLanguage = languagesService.findLanguageByName(language);
         if (guessedLanguage == null) {
             return ResponseEntity.notFound().build();
         }
 
         Guess guess = this.guessService.validateGuess(guessedLanguage);
+
         GuessDto dto = GuessDto.from(guess);
+        if (token != null && !token.isEmpty()) {
+            long score = this.guessService.getScoreFromGuess(guess);
+            User user = usersService.getUserByToken(token);
+            if (user == null) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(dto);
+            }
+            user.setScore(user.getScore() + score);
+            usersService.updateUser(user);
+        }
+
         return ResponseEntity.ok(dto);
     }
 }
